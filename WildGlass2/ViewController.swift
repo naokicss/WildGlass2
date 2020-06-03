a7e357ad01d6086d2426d90473b2fb88b2b9d802
naokihtml <miyaki101033@gmail.com>
2020-06-03 23:29:41 +0900

    Initial Commit

---
+++ WildGlass2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WildGlass2
+//
+//  Created by みや on 2020/06/03.
+//  Copyright © 2020 みや. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
